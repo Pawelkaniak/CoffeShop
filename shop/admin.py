9252af398866dfde3f1ac013ed08aae7be68dd30
PiotrCzpowski <czapio92@gmail.com>
2017-03-22 19:47:03 +0100

    templates added

--- shop/admin.py
+++ shop/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from .models import Product,Category
 
 class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','price','stock','available','created','updated']
+    list_filter = ['available','created','updated']
+    prepopulated_fields = {'slug': ('name',)}
+
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name','slug']
     prepopulated_fields = {'slug': ('name',)}
 
-
-class CategoryADms
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Category,CategoryAdmin)
 
 
 
